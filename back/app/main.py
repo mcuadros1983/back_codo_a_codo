2a855cc129653ac46b6b4a2d365ede91a1b06ac1
mcuadros1983 <106201571+mcuadros1983@users.noreply.github.com>
2023-12-04 22:57:52 -0300

    Update main.py

--- back/app/main.py
+++ back/app/main.py
@@ -1,18 +1,18 @@
 from app import app
 # from products import products
-from dotenv import load_dotenv
-import os
+# from dotenv import load_dotenv
+# import os
 # Rutas
 from routes import products_routes
-    
 
-load_dotenv()  # toma las variables de entorno de .env
+import os
+from dotenv import load_dotenv
+project_folder = os.path.expanduser('~/fullstack_codo_a_codo/back')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
+
+
+# load_dotenv()  # toma las variables de entorno de .env
 
 ruta_raiz = os.path.dirname(os.path.abspath(__file__))
 print("Ruta Raíz:", ruta_raiz)
 # app.register_blueprint(products)
-
-# inicia el servidor web con el framework flask
-if __name__ == "__main__":
-    app.run(port=os.getenv('PORT') , debug=True)
-
