a3941159c8125782901a86533dbe1159654f4613
mcuadros1983 <106201571+mcuadros1983@users.noreply.github.com>
2023-12-04 23:01:00 -0300

    Update db.py

--- back/app/db/db.py
+++ back/app/db/db.py
@@ -1,16 +1,19 @@
 from app import app
 from flask_mysqldb import MySQL
 from dotenv import load_dotenv
-
 import os
-    
-load_dotenv()  # toma las variables de entorno del archivo .env
 
-# Configuraciones de Mysql 
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER') or 'root'
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD') or 'latradicion123'
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST') or '127.0.0.1' # localhost
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB') or 'products_db'
+project_folder = os.path.expanduser('~/fullstack_codo_a_codo/back')
+load_dotenv(os.path.join(project_folder, '.env'))
+
+# import os
+# load_dotenv()  # toma las variables de entorno del archivo .env
+
+# Configuraciones de Mysql
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 # Conexion de MySQL
