e9db2c920b2db931c5c78fcef76c58f1ae9f4691
mcuadros1983 <106201571+mcuadros1983@users.noreply.github.com>
2023-12-04 22:57:31 -0300

    Update app.py

--- back/app/app.py
+++ back/app/app.py
@@ -4,12 +4,11 @@ import os
 
 
 # Inicializacion de la aplicacion
-app = Flask(__name__, static_folder='static', static_url_path='/app/static')
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 # app = Flask(__name__)
 
 # Configuración para la carpeta de carga
-app.config['UPLOAD_FOLDER'] = 'app/static/uploads'
-# app.config['UPLOAD_FOLDER'] = 'public/uploads'
+app.config['UPLOAD_FOLDER'] = 'static'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 
 # Habilitacion de CORS  
